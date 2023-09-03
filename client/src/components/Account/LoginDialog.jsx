b929a76bd7760a7643eb45e3bcc73ad2861d1127
urmila1907 <urmilasangwan744@gmail.com>
2023-09-03 19:30:52 +0530

    working on messenger.jsx and LoginDialog.jsx

---
+++ client/src/components/Account/LoginDialog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginDialog = () => {
+  return (
+    <div>LoginDialog</div>
+  )
+}
+
+export default LoginDialog
---
+++ client/src/components/Messenger.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginDialog from './Account/LoginDialog'
+
+const Messenger = () => {
+  return (
+    <div>
+      <LoginDialog/>
+    </div>
+  )
+}
+
+export default Messenger
